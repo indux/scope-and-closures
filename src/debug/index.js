cbff7f60b961cbe860133609598efe7265601b46
Cristian Da Conceicao <cristiandaconceicao12@gmail.com>
2023-03-17 15:26:31 -0500

    Add Hoisting and Debugs

---
+++ src/debug/index.js
@@ -0,0 +1,8 @@
+var a = "Hello";
+
+function Hello() {
+  let b = "Hello World";
+  const c = "Hello World";
+  debugger; // ACTIVA MODO DEBUGGER EN NAVEGADOR
+}
+Hello();
---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+console.log(nameOfDog); // SE AGREGA UNA VAR = UNDEFINED
+
+var nameOfDog = "Elmo";
+console.log(nameOfDog); // SE LLAMA CORRECTAMENTE
+
+
+
+
+
+function nameOfDog() {
+  console.log(`El mejor perrito es ${elmo}`); // ELMO ES UNDEFINED
+}
+nameOfDog();
+
+var elmo = "Elmito"; // EL PROGAMA NO LO LEE, DEBE ESTAR ARRIBA DEL TODO
